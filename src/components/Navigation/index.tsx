82ae47f8957647ffc07fa32ae91b98a158d3f6aa
May Sunktong <pattaranan.sunktong@gmail.com>
2025-06-06 23:18:51 +0200

    Set condition in Navigation for no login

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -22,24 +22,24 @@ const Navigation = () => {
   return (
     <nav className="nav">
       <div>
-        {username && (
-          <div>
-            <section>
-              <ul>
-                {NavItemList.map((item, index) => (
-                  <li key={index}>
-                    <Link href={item.href}>{item.label}</Link>
-                  </li>
-                ))}
-              </ul>
-            </section>
-            <section>
-              <h1>Hello, {username}!</h1>
-              <button className="button-main" type="button" onClick={logout}>
-                Logout
-              </button>
-            </section>
-          </div>
+        <section>
+          <ul>
+            {NavItemList.map((item, index) => (
+              <li key={index}>
+                <Link href={item.href}>{item.label}</Link>
+              </li>
+            ))}
+          </ul>
+        </section>
+        {username ? (
+          <section>
+            <h1>Hello, {username}!</h1>
+            <button className="button-main" type="button" onClick={logout}>
+              Logout
+            </button>
+          </section>
+        ) : (
+          <h1>Please log in</h1>
         )}
       </div>
     </nav>
